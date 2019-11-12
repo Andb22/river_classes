cb550c518fd7fe34f1842072ce7ad2e95797dd3c
Andrew Binnie <binnieandrew@gmail.com>
2019-11-12 20:55:34 +0000

    Adding fish count and add fish methods

--- river.rb
+++ river.rb
@@ -2,9 +2,18 @@ class River
 
 attr_reader :name, :fish
 
-def initialize(name, fish)
+def initialize(name)
   @name = name
   @fish = []
 end
+
+def fish_count()
+  return @fish.length
+end
+
+def add_fish(fish)
+  @fish.push(fish)
+end
+
 
 end
--- specs/river_specs.rb
+++ specs/river_specs.rb
@@ -1,19 +1,35 @@
 require("MiniTest/autorun")
 require('MiniTest/rg')
 require_relative('../river')
+require_relative('../fish')
 
 class RiverTest < MiniTest::Test
 
   def setup ()
     @river = River.new("Himekawa")
+    @fish = Fish.new("Marlin")
   end
 
-  def test_can_create_river
+  def test_can_create_river()
     assert_equal(River, @river.class())
   end
 
-  def test_return_river_name
-    assert_equal("Himekawa", @river.name)
+  def test_return_river_name()
+    assert_equal("Himekawa", @river.name())
   end
+
+  def test_river_fish_count()
+    assert_equal(0, @river.fish_count)
+  end
+
+  def test_river_add_fish()
+    @river.add_fish(@fish)
+    assert_equal(1, @river.fish_count)
+  end
+
+  # def test_lose_fish()
+  #
+  #   assert_equal(0, @river.)
+  # end
 
 end
